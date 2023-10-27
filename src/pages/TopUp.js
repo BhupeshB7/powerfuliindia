42595859837e13c02e5cf616c6934ed21d247f81
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-10-27 14:54:51 +0530

    update

--- src/pages/TopUp.js
+++ src/pages/TopUp.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Table, Pagination, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table, Pagination, Container } from "react-bootstrap";
 import spinner from "../assets/spinner2.gif";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Topup() {
   const [userData, setUserData] = useState([]);
@@ -34,11 +34,14 @@ function Topup() {
   // Fetch top-up history based on userID
   useEffect(() => {
     if (data.userId) {
-      axios.get(`https://mlm-production.up.railway.app/api/topupHistory/${data.userId}`)
-        .then(response => {
+      axios
+        .get(
+          `https://mlm-production.up.railway.app/api/topupHistory/${data.userId}`
+        )
+        .then((response) => {
           setUserData(response.data);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     }
@@ -49,7 +52,9 @@ function Topup() {
   // Calculate the index of the first item on the current page
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   // Get the current items for the current page
-  const currentItems = Array.isArray(userData) ? userData.slice(indexOfFirstItem, indexOfLastItem) : [];
+  const currentItems = Array.isArray(userData)
+    ? userData.slice(indexOfFirstItem, indexOfLastItem)
+    : [];
 
   // Change page
   const handlePageChange = (pageNumber) => {
@@ -67,28 +72,69 @@ function Topup() {
   return (
     <div>
       {token ? (
-        <div className='topUPBg'>
-          <h4 className='text-center text-success p-4'>TopUp History: {data.userId}</h4>
-          <div className='table-responsive'>
+        <div className="topUPBg">
+          <h4 className="text-center text-success p-4">
+            TopUp History: {data.userId}
+          </h4>
+          <div className="table-responsive">
             <Container>
-              <Table striped bordered hover style={{ border: '2px solid black' }}>
-                {/* Rest of your table rendering code */}
-              </Table>
+              <div className="table-responsive">
+                <Container>
+                  <Table
+                    striped
+                    bordered
+                    hover
+                    style={{ border: "2px solid black" }}
+                  >
+                    <thead>
+                      <tr style={{ color: "#000428" }}>
+                        <th className="text-center">Name</th>
+                        <th className="text-center">Amount</th>
+                        <th className="text-center">UserId</th>
+                        <th className="text-center">Transfer Id</th>
+                        <th>Date</th>
+                        {/* Add more table headers for additional fields */}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currentItems.map((item) => (
+                        <tr key={item._id} className="text-dark">
+                          <td>{item.name}</td>
+                          <td>{item.amount}</td>
+                          <td>{item.userId}</td>
+                          <td>{item.targetUserId}</td>
+                          <td>{item.createdAt}</td>
+                          {/* Add more table cells for additional fields */}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </Container>
+              </div>
             </Container>
           </div>
 
           <Pagination>
-            {Array.from({ length: Math.ceil(userData.length / itemsPerPage) }, (_, index) => (
-              <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => handlePageChange(index + 1)}>
-                {index + 1}
-              </Pagination.Item>
-            ))}
+            {Array.from(
+              { length: Math.ceil(userData.length / itemsPerPage) },
+              (_, index) => (
+                <Pagination.Item
+                  key={index + 1}
+                  active={index + 1 === currentPage}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </Pagination.Item>
+              )
+            )}
           </Pagination>
         </div>
       ) : (
         <>
-          <h6 className="text-center text-secondary">Re-login to continue...</h6>
-          <Link to="/login" style={{ textDecoration: 'underline' }}>
+          <h6 className="text-center text-secondary">
+            Re-login to continue...
+          </h6>
+          <Link to="/login" style={{ textDecoration: "underline" }}>
             <p className="text-center text-primary">Login</p>
           </Link>
         </>
